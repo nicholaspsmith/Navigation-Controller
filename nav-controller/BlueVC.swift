bc6ac3740b2211d148ffc98c3677a989d766db6f
Nicholas Smith <npsmith1990@gmail.com>
2015-12-20 15:37:53 -0600

    Add button to pop to root view

---
+++ nav-controller/BlueVC.swift
@@ -0,0 +1,39 @@
+//
+//  BlueVC.swift
+//  nav-controller
+//
+//  Created by Nick on 12/20/15.
+//  Copyright © 2015 Nick. All rights reserved.
+//
+
+import UIKit
+
+class BlueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func popToRoot(sender: AnyObject) {
+        navigationController?.popToRootViewControllerAnimated(true)
+    }
+
+}

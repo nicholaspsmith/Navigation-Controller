51a848163782c0965a21e49e8c16b01eafb20c8c
Nicholas Smith <npsmith1990@gmail.com>
2015-12-20 15:35:18 -0600

    Programmatically create back button

---
+++ nav-controller/YellowVC.swift
@@ -0,0 +1,38 @@
+//
+//  YellowVC.swift
+//  nav-controller
+//
+//  Created by Nick on 12/20/15.
+//  Copyright © 2015 Nick. All rights reserved.
+//
+
+import UIKit
+
+class YellowVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func popVC(sender: AnyObject) {
+        self.navigationController?.popToRootViewControllerAnimated(true)
+    }
+}
